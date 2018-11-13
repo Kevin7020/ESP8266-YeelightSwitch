12385d4a3e33f6d3d67268b7fbb2d4e810626261
Kevin Porras R <kevin702010@hotmail.com>
2018-11-12 22:10:11 -0600

    Removed fuction using time intervals and minor fixes

--- ESP8266-YeelightSwitch.ino
+++ ESP8266-YeelightSwitch.ino
@@ -2,55 +2,60 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-#define button 12 // Pin for the button
-#define led 14 //Pin for the notificationLed
+#define button 12                //Pin for the button (D6)
+#define LED_BUILTIN 2            //LED_BUILTIN for the notificationLed
 WiFiManager wifiManager;
-String text="{\"id\":1,\"method\":\"toggle\",\"params\":[]}\r\n"; // Function for toggle the light 
-const char* H0 = "192.168.1.106"; // Ip from first light
-const char* H1 = "192.168.1.107"; //Ip from second light if you are using one light comment this line and lines 47,48,49
-int port =55443; // Lights port 
+
+const char* H0 = "192.168.0.50"; // Ip from first light
+//const char* H1 = "192.168.1.107"; //Ip from second light
+
+/*
+ * If you want to make this please make sure to add a PULL-UP resistor 
+ * between the pin 12(D6) and the 3.3v rail (then add the button 12(D6) to GND), 
+ * so that your light does not go full stroboscopic mode :)
+*/
+
 void setup() {
-      pinMode(led, OUTPUT);
-      digitalWrite(led, LOW);
-      pinMode(button, INPUT);
-      Serial.begin(115200);
-      wifiManager.setConfigPortalTimeout(180); 
-      wifiManager.autoConnect("LightSwitch");  
-      Serial.println("Started!");
-      digitalWrite(led, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(button, INPUT);
+  Serial.begin(115200);
+  wifiManager.setConfigPortalTimeout(180);
+  wifiManager.autoConnect("LightSwitch");
+  Serial.println("Started!");
+  digitalWrite(LED_BUILTIN, HIGH);
 }
-void loop() {
-int tt = 0;
-while (digitalRead(button) == LOW && tt < 40) //This is function with option to change the WiFi network if you press the button for ~8 sec you will enter into congif mode.
-{                                             //If you press the button for less of 8 sec you will send toggle to led light.
-    Serial.println(WiFi.status());
-    Serial.println("Reading Seconds!");
-    delay(100);
-    tt++;
-}
-if (tt < 40){
-Serial.println("ShortTime");
-SendInfo();
-}
-else{
-Serial.println("LongTime");
-OnDemandConfig();
-} 
-}
-void SendInfo(){ 
+
+void SendInfo(const char* IpAddress) {
   // Use WiFiClient class to create TCP connections
-  digitalWrite(led, LOW);
+  String TcpMethod = "{\"id\":1,\"method\":\"toggle\",\"params\":[]}\r\n"; // Toggle method for the Yeelight more at https://www.yeelight.com/download/Yeelight_Inter-Operation_Spec.pdf
+  int port = 55443; // Lights port
+  digitalWrite(LED_BUILTIN, LOW);
   WiFiClient client;
-  client.connect(H0,port);
-  client.print(text);
+  Serial.println("Creating a TCP connection");
+  client.connect(IpAddress, port);
+  Serial.println("Sending data through the connection");
+  client.print(TcpMethod);
   client.stop();
-  client.connect(H1,port);
-  client.print(text);
-  client.stop();
-  digitalWrite(led, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
-void OnDemandConfig(){
-      wifiManager.setConfigPortalTimeout(180); //Startuva WiFi vo AP i moze da se prekonfigurira WIFI-ot i novite informacii ke se zacuvaat vo EEPROM.
-      wifiManager.startConfigPortal("LightSwitch"); // Se pushta so ime SmartPost 
+
+void OnDemandConfig() {
+  wifiManager.setConfigPortalTimeout(180);
+  wifiManager.startConfigPortal("LightSwitch");
 }
+
+void loop() {
+  if (digitalRead(button) == LOW) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("STATUS: Connected to AP");
+    } else {
+      Serial.println("STATUS: Disconnected from AP");
+    }
+    Serial.println("Button 12 fired");
+    delay(500);
+    SendInfo(H0);
+  }
+}
+
 
